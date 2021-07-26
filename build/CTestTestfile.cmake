2266bf7551e2568029add084d0e2d25d8214d67f
Christian Pfitzner <christian.pfitzner@thi.de>
2021-07-26 14:16:37 +0200

    -- initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/catkin_ws/src/gazebo_thi
+# Build directory: /home/chris/catkin_ws/src/gazebo_thi/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/chris/catkin_ws/src/gazebo_thi/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/chris/catkin_ws/src/gazebo_thi/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/chris/.local/bin:/home/chris/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/chris/catkin_ws/src/gazebo_thi/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chris/catkin_ws/src/gazebo_thi/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/chris/catkin_ws/src/gazebo_thi:$ROS_PACKAGE_PATH"
